e27022374942962ffea569aaf66f7c73172d6860
Jamal Frank <jamaljfrank@gmail.com>
2019-08-21 03:31:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,10 @@
 def unsafe?(speed)
   speed = 35
-  if speed < 40 || speed > 60
-    puts TRUE
-  else puts FALSE
+  if speed < 40  
+    puts true
+  elsif speed > 60
+    puts true 
+  else puts false
   end
 end
 
